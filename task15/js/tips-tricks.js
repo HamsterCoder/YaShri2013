9a9f71543ad418b553719c64cb57b145497c56cd
Sirinity <sirinity13@yahoo.co.uk>
2013-08-16 06:46:44 +0400

    defeat

--- task15/js/tips-tricks.js
+++ task15/js/tips-tricks.js
@@ -3,19 +3,19 @@
 */
 (function(){
     var
+        
         form = $('#js-application-form'),
         formElementSelector = '.answer__field',
         tipSelector = '.js-tip',
+        currentQuestion = null,
         questionSelector = '.application-form__body__question';
     
-    form.on('focus.tip', formElementSelector, function(evt){
-        $(evt.target).closest(questionSelector).find(tipSelector).slideDown();
+    form.on('click.tip', questionSelector, function(evt){
+        $(evt.currentTarget).closest(questionSelector).find(tipSelector).slideDown();
     });
     
-    form.on('blur.tip', formElementSelector, function(evt){
-        $(evt.target).closest(questionSelector).find(tipSelector).hide();
-    });
-    
-    // TODO bring to order this chaos of css and js hooks.
-    // ERROR with many field for one question it looks ugly; Save current element maybe?
+    // TODO bring to order this chaos of css and js hooks;
+    // ERROR radio buttons do not blur or focus;
+    // ERROR with multiple inputs it get jumpy;
+    // Decided to do a show tip on click functionality;
 })();
--- task15/js/validate.js
+++ task15/js/validate.js
@@ -3,5 +3,59 @@
 */
 
 (function(){
-
+    var
+        form = $('#js-application-form'),
+        inputElementSelector = '.answer__field',
+        fieldTypes = [
+            'number', //0 < length, regex
+            'shortText', //0 < length < 150
+            'link', //0 < length, regex
+            'longText' // 0 < length
+        ],
+        statusSelectorContainer = '.answer__accept-state',
+        statusSelector = {
+            success:'.answer__accept-state_state_success',
+            empty:'.answer__accept-state_state_empty',
+            fail:'.answer__accept-state_state_fail',
+        },
+        questionSelector = '.application-form__body__question';
+    
+    form.on('blur.validate', inputElementSelector, function(evt){
+        var 
+            input = $(evt.target),
+            value = input.val(),
+            validate = 'fail';
+        if(input.hasClass('js-validate-number')) {
+            if(value.length == 0) {
+                validate = 'empty';
+            } else if (/\d+/.test(value)){
+                validate = 'success'
+            }
+        } else if (input.hasClass('js-validate-shortText')) {
+            if(value.length == 0) {
+                validate = 'empty';
+            } else if (value.length <= 150){
+                validate = 'success'
+            }
+        } else if (input.hasClass('js-validate-longText')) {
+            if(value.length == 0) {
+                validate = 'empty';
+            } else {
+                validate = 'success'
+            }
+        } else if (input.hasClass('js-validate-link')) {
+            if(value.length == 0) {
+                validate = 'empty';
+            } else if (/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i.test(value)){
+                validate = 'success'
+            }
+        } else {
+            validate = 'success';
+        }
+        
+        input.closest(questionSelector).find(statusSelectorContainer).children().hide();
+        input.closest(questionSelector).find(statusSelector[validate]).show();
+        
+        input.trigger('validate');
+    });
 })();
